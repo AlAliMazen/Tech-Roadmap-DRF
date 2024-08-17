88fb9f34ed26692cd78a2f3bbf511170e5962596
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-08-17 15:36:32 +0200

    Code: cretaing the Profile Detail and Not Found Code on the API

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -15,4 +16,18 @@ class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+class ProfileDetail(APIView):
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        profile =self.get_object(pk)
+        # because we only want one single profile we don't pass the many arg
+        serializer = ProfileSerializer(profile)
         return Response(serializer.data)
