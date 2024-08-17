e014170ced4277127a0003f618c319f606f4be4c
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-08-17 05:29:13 +0200

    initial Commit : Setting up the project main up with cloudinary Pillow

---
+++ tech_roadmap_root/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for tech_roadmap_root project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tech_roadmap_root.settings')
+
+application = get_asgi_application()
---
+++ tech_roadmap_root/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tech_roadmap_root project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tech_roadmap_root.settings')
+
+application = get_wsgi_application()
