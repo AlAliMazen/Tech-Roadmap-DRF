c0f83ce69e731a42ce55842962171530935dea80
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-08-18 21:17:44 +0200

    Code creating POST function to create a categroy only for the logged in user

--- category/views.py
+++ category/views.py
@@ -13,10 +13,32 @@ class CategoryList(APIView):
     created to list all available categries for the Technology areas 
     e.g. Software Developement, Networking, Cyber-Security, Virtualisation...
     """
+
+    # serializer class for CREATE a Category
+    serializer_class = CategorySerializer
+    
+    # check if the user is logged in and authenticated in order to write a post.
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    
     def get(self, request):
         category = Category.objects.all()
         serializer = CategorySerializer(
             category, many=True, context={"request":request}
         )
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = CategorySerializer(
+            data = request.data, context={"request":request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
 
