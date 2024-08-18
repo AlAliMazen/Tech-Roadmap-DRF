f578d59591dd96e645d9180ea985aa130c950fee
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-08-19 01:07:25 +0200

    Codecreate a POST method to create a new article for authenticated users

--- article/views.py
+++ article/views.py
@@ -11,9 +11,32 @@ class ArticleList(APIView):
     """
     List all written articles
     """
+    # serializer form 
+    serializer_class= ArticleSerializer
+
+    # Onyl authenticated user can write an article otherwise only read
+    permission_classes=[
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    
     def get(self, request):
         article = Article.objects.all()
         serializer = ArticleSerializer(
             article, many=True, context={"request":request}
         )
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ArticleSerializer(
+            data = request.data, context={'request':request}
+        )
+        # check if the coming serializer data are valid
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        # otherwise when wrong
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
