dbf38a3238607c1b7508973e8d526b87dddb7959
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-08-19 04:43:36 +0200

    Code add image validation to article serializer

--- article/serializers.py
+++ article/serializers.py
@@ -7,7 +7,24 @@ class ArticleSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     category_title = serializers.ReadOnlyField(source='category.title' )
-
+    
+    # write a fucntion to validate the image size, width and height
+    # common convension is to call validate_[name_of_field]
+    def validate_image(self, value):
+        if value.size > 1024*1024*2:
+            raise serializers.ValidationError (
+                "Image size is larger than 2MB"
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                "Image width is larger than 4096 inches !"
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                "Image height is larger than 4096 inches !"
+            )
+        return value
+    
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
