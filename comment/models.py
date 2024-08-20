cc890c9272004f83ae40a8670a3adaa4effb7908
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-08-20 02:25:15 +0200

    Code: create the comment Model aloong with its Admin page

---
+++ comment/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from article.models import Article
+
+# Create your models here.
+
+class Comment(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    article= models.ForeignKey(Article, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(blank=False)
+
+    class Meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return f'[{self.id} {self.owner} {self.article.title} {self.content}'
+
+
--- tech_roadmap_root/settings.py
+++ tech_roadmap_root/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'profiles',
     'category',
     'article',
+    'comment',
 ]
 
 MIDDLEWARE = [
