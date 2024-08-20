5aca59b070ba73f35f6b2ca1de40e4fd901a0dfa
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-08-20 18:26:34 +0200

    Code: handling Integriy Error Problem with Like model when postina like to the same article twice

--- likes/serializers.py
+++ likes/serializers.py
@@ -9,10 +9,17 @@ class LikeSerializer(serializers.ModelSerializer):
     The create method handles the unique constraint on 'owner' and 'post'
     """
     owner = serializers.ReadOnlyField(source='owner.username')
-    #article_title = serializers.ReadOnlyField(source='article.title')
 
     class Meta:
         model = Like
         fields = ['id', 'created_at', 'owner','article']
 
-   
+    def create(self, validated_data):
+        try:
+            # create is a method in the super (parent class of ModeSerializer)
+            # that is why we have to use super 
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': "possible duplicate - You can't like an article twice"
+            })
