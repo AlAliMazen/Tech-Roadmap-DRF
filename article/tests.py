b094f8ae2c81e90f9d382adf23dcfda0092a3d92
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-08-20 02:11:30 +0200

    Test: write Test to check if looged in user can create an article

--- article/tests.py
+++ article/tests.py
@@ -8,22 +8,45 @@ from rest_framework.test import APITestCase
 # Create your tests here.
 class ArticleListViewTest(APITestCase):
     def setUp(self):
-        User.objects.create_user(username='thomas', password='!password#')
-         # Create a category
-        Category.objects.create(owner_id=1, title='Test Category')
+        # Create a user
+        self.admin_usr = User.objects.create_user(username='thomas', password='!password#')
+        
+        # Create a category
+        self.category = Category.objects.create(owner=self.admin_usr, title='Test Category')
 
     def test_can_list_articles(self):
-        admin_usr = User.objects.get(username='thomas')
-
-        category = Category.objects.create(title='Test Category', owner=admin_usr)  # Create a Category
-        #Article.objects.create(owner=admin_usr, title='a title',content="thomas content")
+        # Create an article
         Article.objects.create(
-            owner= admin_usr, 
+            owner=self.admin_usr, 
             title='a title', 
             content="thomas content", 
-            category=category
+            category=self.category
         )
+        
+        # Perform GET request to list articles
         response = self.client.get('/articles/')
+        
+        # Check that the response status is 200 OK
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         print(response.data)
         print(len(response.data))
+
+    def test_logged_in_user_can_creat_article(self):
+        # Log in the user with the correct password
+        self.client.login(username='thomas', password='!password#')
+        
+        # Perform POST request to create an article
+        response = self.client.post('/articles/', {
+            'title': 'a title', 
+            'owner': self.admin_usr.id,  # Use the ID of the user
+            'category': self.category.id,  # Use the ID of the category
+            'content': 'any content'
+        })
+        
+        # Check that an article was created
+        count = Article.objects.count()
+        print("Count is ", count)
+        self.assertEqual(count, 1)
+        
+        # Check that the response status is 201 CREATED
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
