9fd8c4af7b365a30814eefd4d6052793e8b20592
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-08-21 00:31:56 +0200

    Code: create followers model with its admin page

---
+++ followers/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Follower(models.Model):
+    """
+    follower gives users possiblity to follow other user
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followed')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'followed']
+
+    def __str__(self):
+        return f'{self.owner} {self.followed}'
+
+
--- tech_roadmap_root/settings.py
+++ tech_roadmap_root/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'article',
     'comment',
     'likes',
+    'followers',
 ]
 
 MIDDLEWARE = [
