a1df7a9509637c2997e18a7badcc78df4368b462
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-08-20 03:06:16 +0200

    Code create CommentList view using Generic Class

--- comment/views.py
+++ comment/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from tech_roadmap_root.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
 
 # Create your views here.
+"""
+Using generic will save much time because it 
+comes with most of the method user need to 
+GET, POST, UPDATE and DELETE
+You need just to learn the syntax
+"""
+
+class CommentList(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnyl]
+    queryset = Comment.object.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
