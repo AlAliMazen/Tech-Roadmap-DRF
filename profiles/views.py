deb7fb7c5ffad9cd13b6a77e2574f81f925b6334
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-08-21 19:01:31 +0200

    Code: create generic View for the Profile for List, update and delete

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
-from rest_framework import status
+from django.db.models import Count
+from rest_framework import generics,filters, status
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -10,57 +11,19 @@ from tech_roadmap_root.permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 
-class ProfileList(APIView):
+class ProfileList(generics.ListAPIView):
     """
     List all profiles
-    No Create view (post method), as profile creation handled by django signals
+    No Create view (post method),
     """
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles, many=True, context={'request':request}
-            )
-        return Response(serializer.data)
-
-class ProfileDetail(APIView):
-    """
-    used to get the profile details
-    """
-    # following is used to create a form from the profile class in the model 
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
-    # check the permission of the each profile
-    permission_classes =[IsOwnerOrReadOnly]
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        profile =self.get_object(pk)
-        # because we only want one single profile we don't pass the many arg
-        serializer = ProfileSerializer(profile, context={'request':request})
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data, context={'request':request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk):
-        try:
-            profile = Profile.object.get(pk=pk)
-
-            if profile.user != request.user:
-                return Response({'Detail':"You don't have permission to delete this profile"},
-                                status=status.HTTP_403_FORBIDDEN)
-            profile.delete()
-            return Response({'Detail':"Profile deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
-        except Profile.DoesNotExist:
-            return Response({'Detail':'Profile not found'}, status=status.HTTP_404_NOT_FOUND)
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, Updte and Destroy a profile
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+   
