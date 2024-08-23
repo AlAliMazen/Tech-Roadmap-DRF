1ff6a3aea7ee6c4fdc3644a78fc69292141950ba
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-08-23 21:01:09 +0200

    Code: FIX: type error in the comment serializer

--- comment/serializers.py
+++ comment/serializers.py
@@ -8,8 +8,8 @@ class CommentSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    created_at = serializers.SerializerMethod()
-    updated_at = serializers.SerializerMethod()
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
     
     def get_is_owner(self, obj):
         request = self.context['request']
