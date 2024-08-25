13076c3dbc93f1c86ad692072d7f768eaa9c7106
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-08-25 12:17:50 +0200

    CONFIG: change the allowed hosts to solve the deployment problem

--- tech_roadmap_root/settings.py
+++ tech_roadmap_root/settings.py
@@ -70,7 +70,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost','tech_road_map.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
